e1b62eaf463e2328206aa952288752aa1de99630
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-01-19 13:07:57 +0530

    Update Counting_sort_II.py

--- Problem_Solving/Counting_sort_II.py
+++ Problem_Solving/Counting_sort_II.py
@@ -1,19 +1,23 @@
-def countingSort(array):
-    size = len(array)
-    output = [0] * size
-    count = [0] * 10
-    for i in range(0, size):
-        count[array[i]] += 1
-    for i in range(1, 10):
-        count[i] += count[i - 1]
-    i = size - 1
-    while i >= 0:
-        output[count[array[i]] - 1] = array[i]
-        count[array[i]] -= 1
-        i -= 1
-    for i in range(0, size):
-        array[i] = output[i]
+def counting_sort(alist, largest):
+    c = [0]*(largest + 1)
+    for i in range(len(alist)):
+        c[alist[i]] = c[alist[i]] + 1
+    c[0] = c[0] - 1 
+    for i in range(1, largest + 1):
+        c[i] = c[i] + c[i - 1]
+ 
+    result = [None]*len(alist)
+
+    for x in reversed(alist):
+        result[c[x]] = x
+        c[x] = c[x] - 1
+ 
+    return result
+ 
+ 
 n=int(input())
 a=list(map(int,input().split()))
-countingsort(a)
-print(' '.join(map(str,a)))
+k = max(a)
+A = counting_sort(a, k)
+print(' '.join(map(str,A)))
+
